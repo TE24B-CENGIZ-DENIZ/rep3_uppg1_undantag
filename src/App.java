70d883a650da1ccebbeb77fe64cf9af307855cb0
Johan Kateby <johanerik.kateby@ntig.se>
2026-08-28 11:30:56 +0200

    ändrat out/in till IO

--- src/App.java
+++ src/App.java
@@ -3,53 +3,49 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args)  {
-        Scanner tb = new Scanner(System.in);
+
         int[] array = new int[4];
-                            System.out.println(Arrays.toString(array));
+                            IO.println(Arrays.toString(array));
 
         int val=0;
         while(val!=4)
         {
-            System.out.println("MENY: ");
-            System.out.println("1. Lägg in tal i arrayen på en viss plats");
-            System.out.println("2. Nollställ ett tal i arrayen på en viss plats");
-            System.out.println("3. Flytta ett tal i arrayen från en plats till en annan,");
-            System.out.println("4. Avsluta");
+            IO.println("MENY: ");
+            IO.println("1. Lägg in tal i arrayen på en viss plats");
+            IO.println("2. Nollställ ett tal i arrayen på en viss plats");
+            IO.println("3. Flytta ett tal i arrayen från en plats till en annan,");
+            IO.println("4. Avsluta");
             int tal =0;
             int plats=0;
             int ny_plats =0;
-            System.out.println("Array:"+Arrays.toString(array));
-            System.out.print("Ange val:");
-            val=tb.nextInt();
+            IO.println("Array:"+Arrays.toString(array));
+            IO.print("Ange val:");
+            val=Integer.parseInt(IO.readln("Ange val:"));
 
             switch(val){
                   case 1 -> {
-                      System.out.print("Ange tal:");
-                      tal = tb.nextInt();
-                      System.out.print("Ange plats:");
-                      plats = tb.nextInt();
+                      tal = Integer.parseInt(IO.readln("Ange tal:"));
+                      IO.print("Ange plats:");
+                      plats = Integer.parseInt(IO.readln("Ange plats:"));
                       array[plats]=tal;
                 }
                   case 2 -> {
-                      System.out.print("Ange plats som ska nollställas:");
-                      plats = tb.nextInt();
+                      plats = Integer.parseInt(IO.readln("Ange plats som ska nollställas:"));
                       array[plats]=0;
                 }
                   case 3 -> {
-                      System.out.print("Ange plats för tal som ska flyttas:");
-                      plats = tb.nextInt();
-                      System.out.print("Ange ny plats som talet  ska flyttas till:");
+                      plats = Integer.parseInt(IO.readln("Ange plats för tal som ska flyttas:"));
+                      IO.print("Ange ny plats som talet  ska flyttas till:");
                       int temp = array[ny_plats];
                       array[ny_plats]=array[plats];
                       array[plats]=temp;
                 }
-                  case 4 -> System.out.println("Ajöken");
+                  case 4 -> IO.println("Ajöken");
 
-                  default -> System.out.println("Fel val, välj en siffra 1-4");
+                  default -> IO.println("Fel val, välj en siffra 1-4");
             }
                 
         }
-        tb.close();
     }
 
     
